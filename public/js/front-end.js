a75052996c8b8e5833588662a2a635ba2a2aaa2a
Nicole Rosario <nkrosario@rogers.com>
2018-01-25 21:51:44 -0500

    Added ajaxSetup example to front-end.js

--- public/js/front-end.js
+++ public/js/front-end.js
@@ -1,3 +1,11 @@
+// Set up variables to have in every ajax request
+$.ajaxSetup({
+    headers: {
+        userName: 'put_username_cookie_here',
+        password: 'put_password_cookie_here'
+    }
+});
+
 $.ajax({
     type: "POST",
     url: "/test",
